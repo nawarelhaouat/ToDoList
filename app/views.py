985c83e590ba2d23abfbade66245a548ec2ca0ff
nawarelhaouat <nawarelhaouat@gmail.com>
2026-05-12 10:19:15 +0100

    implement create and read ToDoList

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset         = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        qs     = super().get_queryset()
+        status = self.request.query_params.get('status')
+        if status == 'done':
+            qs = qs.filter(completed=True)
+        elif status == 'pending':
+            qs = qs.filter(completed=False)
+        return qs
